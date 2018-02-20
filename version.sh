b5fab51fc97b0f6d3b5ade28231a534ed2b7b307
soukainabakrim <sbakrim@octo.com>
2018-02-20 15:22:07 +0100

    remove file

--- version.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$1
-rx='^([0-9]+\.){2}(\*|[0-9]+)$'
-if [[ $VERSION =~ $rx ]]; then
- echo "INFO:<-->Version $VERSION"
-else
- echo "ERROR:<->Unable to validate package version: '$VERSION'"
- exit 1
-fi
