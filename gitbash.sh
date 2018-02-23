22e11594edadff31a3a4760965a886cea9cd6c6d
soukainabakrim <sbakrim@octo.com>
2018-02-23 11:51:04 +0100

    Check first if Changelog was updated : ## [Unreleased] ==> ## [tag]

--- gitbash.sh
+++ gitbash.sh
@@ -8,68 +8,77 @@
 
 NEW_TAG=$1
 
-# Check if the format of new tag is valid, must match "major.minor.patch"
-rx='^([0-9]+\.){2}(\*|[0-9]+)$'
-if [[ $NEW_TAG =~ $rx ]]
+# Check first if Changelog was updated with release tag
+read -r -p "Did you update Changelog with the new tag? [y/N] " RESPONSE
+if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY])+$ ]]
 then
-    # Get git last tag
-    LAST_TAG=$(git tag | tail -1 | tr -d v)
-
-    # Compare new and last tags
-    if [[ $NEW_TAG == $LAST_TAG ]]
+    # Check if the format of new tag is valid, must match "major.minor.patch"
+    rx='^([0-9]+\.){2}(\*|[0-9]+)$'
+    if [[ $NEW_TAG =~ $rx ]]
     then
-        echo "ERROR:The new tag must be greater than the last tag $LAST_TAG"
-        exit 1
-    fi
+        # Get git last tag
+        LAST_TAG=$(git tag | tail -1 | tr -d v)
 
-    # If last and new tags not equals, transform them to list and compare their digits
-    NEW_TAG_LIST=(${NEW_TAG//\./ })
-    LAST_TAG_LIST=(${LAST_TAG//\./ })
-    for i in 0 1 2
-    do
-        if [[ ${NEW_TAG_LIST[${i}]} -lt ${LAST_TAG_LIST[${i}]} ]]
+        # Compare new and last tags
+        if [[ $NEW_TAG == $LAST_TAG ]]
         then
             echo "ERROR:The new tag must be greater than the last tag $LAST_TAG"
             exit 1
-        elif [[ ${NEW_TAG_LIST[${i}]} -gt ${LAST_TAG_LIST[${i}]} ]]
+        fi
+
+        # If last and new tags not equals, transform them to list and compare their digits
+        NEW_TAG_LIST=(${NEW_TAG//\./ })
+        LAST_TAG_LIST=(${LAST_TAG//\./ })
+        for i in 0 1 2
+        do
+            if [[ ${NEW_TAG_LIST[${i}]} -lt ${LAST_TAG_LIST[${i}]} ]]
+            then
+                echo "ERROR:The new tag must be greater than the last tag $LAST_TAG"
+                exit 1
+            elif [[ ${NEW_TAG_LIST[${i}]} -gt ${LAST_TAG_LIST[${i}]} ]]
+            then
+                break
+            else
+                continue
+            fi
+        done
+
+        read -r -p "Are you sure to add new tag v$NEW_TAG and push to qualif? [y/N] " RESPONSE
+        if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY])+$ ]]
         then
-            break
+            echo "Checkout to Master"
+            git checkout master
+            # Create new tag in local
+            git tag v$NEW_TAG
+
+            # Push the new tag to remote
+            output=$(git push origin v$NEW_TAG 2>&1)
+            # If error delete local tag
+            if [[ $output == fatal* ]]
+            then
+                echo $output
+                git tag -d v$NEW_TAG
+                exit 1
+            fi
+            echo "New git tag added : v$NEW_TAG"
+
+            # Push Master in Qualif
+            output=$(git push -f origin master:qualif 2>&1)
+            if [[ $output == fatal* ]]
+            then
+                echo $output
+                echo "Push to Qualif didn't work, please resolve the error above"
+                exit 1
+            fi
+            echo "Master was pushed in Qualif"
+
         else
-            continue
+            echo "Add tag canceled"
         fi
-    done
-
-    read -r -p "Are you sure to add new tag v$NEW_TAG and push to qualif? [y/N] " RESPONSE
-    if [[ "$RESPONSE" =~ ^([yY][eE][sS]|[yY])+$ ]]
-    then
-        echo "Checkout to Master"
-        git checkout master
-        git tag v$NEW_TAG # Create new tag in local
-
-        # Push the new tag to remote
-        output=$(git push origin v$NEW_TAG 2>&1)
-        # If error delete local tag
-        if [[ $output == fatal* ]]
-        then
-            echo $output
-            git tag -d v$NEW_TAG
-            exit 1
-        fi
-        echo "New git tag added : v$NEW_TAG"
-
-        # Push Master in Qualif
-        output=$(git push -f origin master:qualif 2>&1)
-        if [[ $output == fatal* ]]
-        then
-            echo $output
-            echo "Push to Qualif didn't work, please resolve the error above"
-            exit 1
-        fi
-        echo "Master was pushed in Qualif"
-
     else
-        echo "Add tag canceled"
+        echo "ERROR:Please enter a three digit version, example 2.3.0"
     fi
+
 else
-    echo "ERROR:Please enter a three digit version, example 2.3.0"
+    echo "Please update first the Changelog with the new release tag"
 fi
